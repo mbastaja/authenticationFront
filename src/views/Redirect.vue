a6fcae3f027ed3273785618c6b5e0f312d2f3cf0
mbastaja <bastajam@gmail.com>
2021-10-22 02:08:54 +0200

    add new feature - redirect page - view

---
+++ src/views/Redirect.vue
@@ -0,0 +1,12 @@
+<template>
+  <app-profile-redirect />
+</template>
+<script>
+import Redirect from "@/components/Redirect.vue";
+
+export default {
+  components: {
+    "app-profile-redirect": Redirect,
+  },
+};
+</script>
